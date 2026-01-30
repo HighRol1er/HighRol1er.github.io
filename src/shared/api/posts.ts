e0eab37d265314d908f8a0921e602c2bd44227bb
HighRol1er <hunterjoe9999@gmail.com>
2026-01-30 13:27:05 +0900

    feat: fetchArticle with unit tests

--- src/shared/api/posts.ts
+++ src/shared/api/posts.ts
@@ -1,18 +1,31 @@
 import type { PostMetadata } from "@/types";
 
+/**
+ * Fetch post MetaData
+ */
 export async function fetchPosts(): Promise<PostMetadata[]> {
-  try {
-    const response = await fetch("/posts.json");
+  const response = await fetch("/posts.json");
 
-    if (!response.ok) {
-      console.error("Failed to fetch posts.json:", response.statusText);
-      return [];
-    }
-
-    const posts: PostMetadata[] = await response.json();
-    return posts;
-  } catch (error) {
-    console.error("Error fetching posts:", error);
+  if (!response.ok) {
+    console.error("Failed to fetch posts.json:", response.statusText);
     return [];
   }
+
+  const posts: PostMetadata[] = await response.json();
+  return posts;
+}
+
+/**
+ * Fetch specific article
+ */
+export async function fetchArticle(fileName: string): Promise<string | null> {
+  const response = await fetch(`/_posts/${fileName}`);
+
+  if (!response.ok) {
+    console.error("Failed to fetch article", response.statusText);
+    return null;
+  }
+
+  const article = await response.text();
+  return article;
 }
--- src/shared/api/tests/posts.test.ts
+++ src/shared/api/tests/posts.test.ts
@@ -1,8 +1,9 @@
 import { test, expect, vi } from "vitest";
-import { fetchPosts } from "@/shared/api/posts";
+import { fetchPosts, fetchArticle } from "@/shared/api/posts";
 import { mockPosts } from "@/shared/mocks/posts.mock";
+import { articeMock } from "@/shared/mocks/article.mock";
 
-test("게시글 불러오기 성공", async () => {
+test("등록된 게시글 메타데이터 불러오기 성공", async () => {
   globalThis.fetch = vi.fn().mockResolvedValue({
     ok: true,
     json: async () => mockPosts,
@@ -12,7 +13,7 @@ test("게시글 불러오기 성공", async () => {
   expect(response).toEqual(mockPosts);
 });
 
-test("게시글 불러오기 실패", async () => {
+test("등록된 게시글 메타데이터 불러오기 실패", async () => {
   globalThis.fetch = vi.fn().mockResolvedValue({
     ok: false,
     statusText: "Internal Server Error",
@@ -21,4 +22,29 @@ test("게시글 불러오기 실패", async () => {
   const response = await fetchPosts();
 
   expect(response).toEqual([]);
+});
+
+// 아래서부터는 특정 아티클 가져오는 로직임
+
+test("특정 게시글 본문 가져오기 - 성공", async () => {
+  const path = "/_post";
+  globalThis.fetch = vi.fn().mockResolvedValue({
+    ok: true,
+    text: async () => articeMock,
+  });
+  const response = await fetchArticle(path);
+
+  expect(response).toEqual(articeMock);
+});
+
+test("특정 게시글 본문 가져오기 - 실패", async () => {
+  const path = "/_post";
+  globalThis.fetch = vi.fn().mockResolvedValue({
+    ok: false,
+    statusText: "Internal Srver Error",
+    return: null,
+  });
+  const response = await fetchArticle(path);
+
+  expect(response).toEqual(null);
 });
