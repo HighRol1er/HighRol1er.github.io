7103bc774313e3c41fa26ad2f79a5bd53caf0fdb
HighRol1er <hunterjoe9999@gmail.com>
2026-01-30 13:27:35 +0900

    rename: post -> postMeta

--- src/components/postDetail/PostStats.tsx
+++ src/components/postDetail/PostStats.tsx
@@ -2,15 +2,15 @@ import type { PostMetadata } from "@/types";
 
 import { Date, Tags } from "@/components/posts";
 
-export const PostStats = ({ post }: { post: PostMetadata }) => {
+export const PostStats = ({ postMeta }: { postMeta: PostMetadata }) => {
   return (
     <div className="mb-6 pb-6 border-primary border-b-2">
       <div className="flex flex-wrap gap-2 mb-4">
-        {post.tags.map((tag, index) => (
+        {postMeta.tags.map((tag, index) => (
           <Tags key={index}>{tag}</Tags>
         ))}
       </div>
-      <Date date={post.timestamp} />
+      <Date date={postMeta.timestamp} />
     </div>
   );
 };
