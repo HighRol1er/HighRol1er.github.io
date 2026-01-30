f1e10294fb9f0d8f0210dfa287c78f36e2fcd1cc
HighRol1er <hunterjoe9999@gmail.com>
2026-01-30 18:12:19 +0900

    refactor: loader 추가, 네이밍 변경

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,27 +1,23 @@
+import { Loader } from "@/components/common";
 import { DefaultLayout, Header } from "@/components/layouts";
 import { PostCard } from "@/components/posts/PostCard";
 import { useFetchPosts } from "@/hooks";
-import { useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 
 export const PostPage = () => {
-  const { posts, loading } = useFetchPosts();
+  const { posts, isLoading } = useFetchPosts();
   const [searchParams] = useSearchParams();
   const tagParams = searchParams.get("tag");
 
-  const filteredPosts = useMemo(() => {
-    return tagParams ? posts.filter((p) => p.tags.includes(tagParams)) : posts;
-  }, [posts, tagParams]);
+  const filteredByTag = tagParams
+    ? posts.filter((p) => p.tags.includes(tagParams))
+    : posts;
 
-  if (loading) {
-    return (
-      <div className="text-center py-8 text-muted-foreground">
-        포스트를 불러오는 중...
-      </div>
-    );
+  if (isLoading) {
+    return <Loader />;
   }
 
-  if (filteredPosts.length === 0) {
+  if (filteredByTag.length === 0) {
     return (
       <DefaultLayout>
         <Header title="Posts" />
@@ -37,13 +33,11 @@ export const PostPage = () => {
   return (
     <DefaultLayout>
       <Header title="Posts" />
-      <section>
-        <div className="max-w-7xl mx-auto">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 sm:gap-2">
-            {filteredPosts.map((post, index) => (
-              <PostCard key={post.fileName} post={post} index={index} />
-            ))}
-          </div>
+      <section className="max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 sm:gap-2">
+          {filteredByTag.map((post, index) => (
+            <PostCard key={post.fileName} post={post} index={index} />
+          ))}
         </div>
       </section>
     </DefaultLayout>
